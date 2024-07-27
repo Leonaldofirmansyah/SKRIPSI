0f8f0ea9d3bf79a5326b7c27557b4201ce752ade
Leonaldo Firmansyah <leonaldofirmansyah11@gmail.com>
2024-07-28 00:10:22 +0700

    update

--- konsumen/keranjang.php
+++ konsumen/keranjang.php
@@ -112,7 +112,7 @@ $pesanan = $transaksi->getCartItems();
                 <a class="nav-link" href="profile.php">Profile</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="logout.php">Logout</a>
+                <a class="nav-link" href="../logout.php">Logout</a>
             </li>
         </ul>
     </div>
